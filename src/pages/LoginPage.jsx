eb3c9dc7cad80ddd16c5ff6d8acf880cc4736412
Cesar Segovia <cesarr.segovia27@gmail.com>
2024-08-12 19:16:58 -0300

    funcionando login/user, trabajar estilos

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,9 +4,8 @@ import { useForm } from '../hooks/useForms';
 export const LoginPage = () => {
     const navigate = useNavigate()
 
-    const { name, email, password, onInputChange, onResetForm } = useForm({
+    const { name, password, onInputChange, onResetForm } = useForm({
         name: '',
-        // email: '',
         password: ''
     })
 
@@ -38,10 +37,6 @@ export const LoginPage = () => {
                     <input type="text" name="name" id="name" value={name} onChange={onInputChange} required autoComplete='off' />
                     <label htmlFor="name">Nombre de Usuario:</label>
                 </div>
-                {/* <div className="input-group">
-                    <input type="email" name="email" id="email" value={email} onChange={onInputChange} required autoComplete='off' />
-                    <label htmlFor="email">Email:</label>
-                </div> */}
                 <div className="input-group">
                     <input type="password" name="password" id="password" value={password} onChange={onInputChange} required autoComplete='off' />
                     <label htmlFor="password">Contraseña:</label>
