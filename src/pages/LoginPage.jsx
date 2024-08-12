2033b677c28fb6956f8a403c5aeebbb865a3615b
Cesar Segovia <cesarr.segovia27@gmail.com>
2024-08-12 19:13:55 -0300

    funcionando login/user, trabajar estilos

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,52 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from '../hooks/useForms';
 
 export const LoginPage = () => {
-    const [username, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const { name, email, password, onInputChange, onResetForm } = useForm({
+        name: '',
+        // email: '',
+        password: ''
+    })
 
-        if(username === 'admin' && password === 'turnero'){
-            navigate('/user');
-        }else{
-            alert('Datos incorrectos');
+    const onLogin = (e) => {
+        e.preventDefault()
+
+        if(name === 'admin' && password === 'barbero'){
+            navigate('/user', {
+                replace: true,
+                state: {
+                    logged: true,
+                    name
+                }
+            })
+        } else{
+            alert('Usuario incorrecto')
         }
-    }
-    return (
-        <div>
-            <h1>Acceder</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Usuario: </label>
-                <input type="text" value={username} onChange={(e) => setUserName(e.target.value)} />
-                <label>Password: </label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type='submit'>Login</button>
+
+        
+
+        onResetForm();
+}
+
+    return(
+        <div className="wrapper">
+            <form onSubmit={onLogin}>
+                <h1>Iniciar sesion</h1>
+                <div className="input-group">
+                    <input type="text" name="name" id="name" value={name} onChange={onInputChange} required autoComplete='off' />
+                    <label htmlFor="name">Nombre de Usuario:</label>
+                </div>
+                {/* <div className="input-group">
+                    <input type="email" name="email" id="email" value={email} onChange={onInputChange} required autoComplete='off' />
+                    <label htmlFor="email">Email:</label>
+                </div> */}
+                <div className="input-group">
+                    <input type="password" name="password" id="password" value={password} onChange={onInputChange} required autoComplete='off' />
+                    <label htmlFor="password">Contraseña:</label>
+                </div>
+                <button>Entrar</button>
             </form>
         </div>
     )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import  {LoginPage, UserPage, Hero, Home}  from "../pages";
+import { PrivateRoute } from "./PrivateRoute";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/user" element={<UserPage/>}/>
             <Route path="/home" element={<Home />} />
             <Route path="*" element={<h1>Not Found</h1>} />
             <Route path="/app" element={<App />} />
+            <Route path="/user" element={
+                <PrivateRoute>
+                    <UserPage/>
+                </PrivateRoute>
+            }/>
         </Routes>
     );
 };
